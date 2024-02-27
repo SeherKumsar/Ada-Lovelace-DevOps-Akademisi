603d8704d7df6d166acc677f0df6737936ef44e3
SeherKumsar <seherkumsar99@outlook.com>
2024-02-28 02:23:39 +0300

    added post for assigment1

--- assigment1/app.js
+++ assigment1/app.js
@@ -17,6 +17,80 @@ app.get("/students", (req, res) => {
   });
 });
 
+app.post("/students/add/:name/:age/:midterm_grade/:final_grade", (req, res) => {
+  dbConnection.query("INSERT INTO students (name, age, midterm_grade, final_grade) VALUES (?, ?, ?, ?)",
+  [req.params.name, req.params.age, req.params.midterm_grade, req.params.final_grade], (err, results, fields) => {
+    if (err) {
+      console.log("Database query error: ", err);
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: results,
+      });
+    }
+  });
+});
+
+app.get("/students/add/:name/:age/:midterm_grade/:final_grade", (req, res) => {
+  dbConnection.query("INSERT INTO students (name, age, midterm_grade, final_grade) VALUES (?, ?, ?, ?)",
+  [req.params.name, req.params.age, req.params.midterm_grade, req.params.final_grade], (err, results, fields) => {
+    if (err) {
+      console.log("Database query error: ", err);
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: results,
+      });
+    }
+  });
+});
+
+app.delete("/students/delete/:id", (req, res) => {
+  dbConnection.query("DELETE FROM students WHERE id = ?",
+  [req.params.id], (err, results, fields) => {
+    if (err) {
+      console.log("Database query error: ", err);
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: results,
+      });
+    }
+  });
+});
+
+app.get("/students/delete/:id", (req, res) => {
+  dbConnection.query("DELETE FROM students WHERE id = ?",
+  [req.params.id], (err, results, fields) => {
+    if (err) {
+      console.log("Database query error: ", err);
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: results,
+      });
+    }
+  });
+});
+
+app.get("/students/average_grade/:id", (req, res) => {
+  dbConnection.query("SELECT (midterm_grade + final_grade) / 2 AS average_grade FROM students WHERE id = ?",
+  [req.params.id], (err, results, fields) => {
+    if (err) {
+      console.log("Database query error: ", err);
+    } else {
+      const averageGrade = parseFloat(results[0].average_grade).toFixed(2);
+      res.status(200).json({
+        status: "success",
+        // data: results,
+        data: {
+          average_grade: averageGrade,
+        }
+      });
+    }
+  });
+});
+
 app.get("/students/:id", (req, res) => {
   dbConnection.query("SELECT * FROM students WHERE id = ?",
   [req.params.id], (err, results, fields) => {
